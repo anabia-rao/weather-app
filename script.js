a5a709bc3093be8ba05488264ec4d19742f211af
Taha Rao <rao917120@gmail.com>
2026-06-17 19:32:38 +0500

    icons

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-//await se interpreter waite karega jab tak api se response nahi aata; 
+//await se interpreter waite karega jab tak api se response nahi aata;
 // console.log("getweather function called");
 //queryselecter se city input ka value le lo;
 //axios se api call karo aur response lo;
 //
 async function getweather(event) {
-    event.preventDefault();
-    let city = document.querySelector("#city-input").value;
-    
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+
   let response = await axios(
     `https://api.weatherapi.com/v1/current.json?key=25175e31b7074cfc895204529222906&q=${city}`,
   );
-  console.log("response : ",response);
+  console.log("response : ", response);
   console.log("temp: ", response.data.current.temp_c);
 
   let temperature = response.data.current.temp_c;
   let description = response.data.current.condition.text;
-
-  document.querySelector("#temperature").innerHTML = `Temperature: ${temperature}°C`;
-  document.querySelector("#description").innerHTML = `Description: ${description}`;
+ document.querySelector("#img-tag").src = response.data.current.condition.icon;
+  document.querySelector("#temperature").innerHTML =
+    `Temperature:  ${temperature}°C`;
+  document.querySelector("#description").innerHTML =
+    `Description: ${description}`;
 }
-if (temperature > 30) {
-    document.body.style.backgroundImage.innerHTML = "url('./  images/ChatGPT Image Jun 16, 2026, 02_19_53 PM.png')";
-  } else if (temperature > 20) {
-    document.body.style.backgroundImage = "url('./images/pleasant-weather.jpg')";
-  } else if (temperature > 10) {
-    document.body.style.backgroundImage = "url('./images/cool-weather.jpg')";
-  }
